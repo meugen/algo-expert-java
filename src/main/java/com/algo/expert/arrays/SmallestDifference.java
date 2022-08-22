6fa71f448665d56839963aec1aaaede490f46353
Yevhen Malyshev <yevhen.malyshev@oracle.com>
2022-08-22 22:57:20 +0300

    Added tests for SmallestDifference task

---
+++ src/main/java/com/algo/expert/arrays/SmallestDifference.java
@@ -0,0 +1,36 @@
+package com.algo.expert.arrays;
+
+import java.util.Arrays;
+
+// https://www.algoexpert.io/questions/smallest-difference
+public class SmallestDifference {
+
+    public static int[] smallestDifference(int[] arrayOne, int[] arrayTwo) {
+        Arrays.sort(arrayTwo);
+
+        int[] result = new int[] { arrayOne[0], findItemWithSmallestDifference(arrayTwo, arrayOne[0]) };
+        for (int i=1; i<arrayOne.length; i++) {
+            int itemOne = arrayOne[i];
+            int itemTwo = findItemWithSmallestDifference(arrayTwo, itemOne);
+            if (Math.abs(result[0]-result[1]) > Math.abs(itemOne-itemTwo)) {
+                result[0] = itemOne;
+                result[1] = itemTwo;
+            }
+        }
+        return result;
+    }
+
+    private static int findItemWithSmallestDifference(int[] array, int target) {
+        int index = 0;
+        int difference = Math.abs(target-array[0]);
+        while (index < array.length-1) {
+            int newDifference = Math.abs(target-array[index+1]);
+            if (newDifference > difference) {
+                break;
+            }
+            index++;
+            difference = newDifference;
+        }
+        return array[index];
+    }
+}
---
+++ src/test/java/com/algo/expert/arrays/SmallestDifferenceTest.java
@@ -0,0 +1,33 @@
+package com.algo.expert.arrays;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+
+public class SmallestDifferenceTest {
+
+    @ParameterizedTest
+    @MethodSource("params")
+    void testCases(int[] arrayOne, int[] arrayTwo, int[] expected) {
+        int[] result = SmallestDifference.smallestDifference(arrayOne, arrayTwo);
+        Assertions.assertArrayEquals(expected, result);
+    }
+
+    static List<Arguments> params() {
+        return List.of(
+                Arguments.of(new int[] {-1, 5, 10, 20, 28, 3}, new int[] {26, 134, 135, 15, 17}, new int[] {28, 26}),
+                Arguments.of(new int[] {-1, 5, 10, 20, 3}, new int[] {26, 134, 135, 15, 17}, new int[] {20, 17}),
+                Arguments.of(new int[] {10, 0, 20, 25}, new int[] {1005, 1006, 1014, 1032, 1031}, new int[] {25, 1005}),
+                Arguments.of(new int[] {10, 0, 20, 25, 2200}, new int[] {1005, 1006, 1014, 1032, 1031}, new int[] {25, 1005}),
+                Arguments.of(new int[] {10, 0, 20, 25, 2000}, new int[] {1005, 1006, 1014, 1032, 1031}, new int[] {2000, 1032}),
+                Arguments.of(new int[] {240, 124, 86, 111, 2, 84, 954, 27, 89}, new int[] {1, 3, 954, 19, 8}, new int[] {954, 954}),
+                Arguments.of(new int[] {0, 20}, new int[] {21, -2}, new int[] {20, 21}),
+                Arguments.of(new int[] {10, 1000}, new int[] {-1441, -124, -25, 1014, 1500, 660, 410, 245, 530}, new int[] {1000, 1014}),
+                Arguments.of(new int[] {10, 1000, 9124, 2142, 59, 24, 596, 591, 124, -123}, new int[] {-1441, -124, -25, 1014, 1500, 660, 410, 245, 530}, new int[] {-123, -124}),
+                Arguments.of(new int[] {10, 1000, 9124, 2142, 59, 24, 596, 591, 124, -123, 530}, new int[] {-1441, -124, -25, 1014, 1500, 660, 410, 245, 530}, new int[] {530, 530})
+        );
+    }
+}
