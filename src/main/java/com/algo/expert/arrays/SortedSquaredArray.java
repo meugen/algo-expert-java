f2cf84aface88716d63bfe683e79674e7440de63
Yevhen Malyshev <yevhen.malyshev@oracle.com>
2022-08-22 18:47:57 +0300

    Added tests for SortedSquaredArray task

---
+++ src/main/java/com/algo/expert/arrays/SortedSquaredArray.java
@@ -0,0 +1,17 @@
+package com.algo.expert.arrays;
+
+import java.util.Arrays;
+
+public class SortedSquaredArray {
+
+    private SortedSquaredArray() {}
+
+    public static int[] sortedSquaredArray(int[] array) {
+        int[] result = new int[array.length];
+        for (int i=0; i<array.length; i++) {
+            result[i] = array[i] * array[i];
+        }
+        Arrays.sort(result);
+        return result;
+    }
+}
---
+++ src/test/java/com/algo/expert/arrays/SortedSquaredArrayTest.java
@@ -0,0 +1,39 @@
+package com.algo.expert.arrays;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+
+public class SortedSquaredArrayTest {
+
+    @ParameterizedTest
+    @MethodSource("params")
+    void testCases(int[] array, int[] expected) {
+        int[] result = SortedSquaredArray.sortedSquaredArray(array);
+        Assertions.assertArrayEquals(expected, result);
+    }
+
+    static List<Arguments> params() {
+        return List.of(
+                Arguments.of(new int[] {1, 2, 3, 5, 6, 8, 9}, new int[] {1, 4, 9, 25, 36, 64, 81}),
+                Arguments.of(new int[] {1}, new int[] {1}),
+                Arguments.of(new int[] {1, 2}, new int[] {1, 4}),
+                Arguments.of(new int[] {1, 2, 3, 4, 5}, new int[] {1, 4, 9, 16, 25}),
+                Arguments.of(new int[] {0}, new int[] {0}),
+                Arguments.of(new int[] {10}, new int[] {100}),
+                Arguments.of(new int[] {-1}, new int[] {1}),
+                Arguments.of(new int[] {-2, -1}, new int[] {1, 4}),
+                Arguments.of(new int[] {-5, -4, -3, -2, -1}, new int[] {1, 4, 9, 16, 25}),
+                Arguments.of(new int[] {-10}, new int[] {100}),
+                Arguments.of(new int[] {-10, -5, 0, 5, 10}, new int[] {0, 25, 25, 100, 100}),
+                Arguments.of(new int[] {-7, -3, 1, 9, 22, 30}, new int[] {1, 9, 49, 81, 484, 900}),
+                Arguments.of(new int[] {-50, -13, -2, -1, 0, 0, 1, 1, 2, 3, 19, 20}, new int[] {0, 0, 1, 1, 1, 4, 4, 9, 169, 361, 400, 2500}),
+                Arguments.of(new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}),
+                Arguments.of(new int[] {-1, -1, 2, 3, 3, 3, 4}, new int[] {1, 1, 4, 9, 9, 9, 16}),
+                Arguments.of(new int[] {-3, -2, -1}, new int[] {1, 4, 9})
+        );
+    }
+}
