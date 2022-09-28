1e5bc3a0335634b26be04d5dabfa77b1dd4c1d17
Yevhen Malyshev <yevhen.malyshev@oracle.com>
2022-09-28 21:54:20 +0300

    Refactored BinarySearch task

--- src/main/java/com/algo/expert/searching/BinarySearch.java
+++ src/main/java/com/algo/expert/searching/BinarySearch.java
@@ -1,18 +1,23 @@
 package com.algo.expert.searching;
 
-public class BinarySearch {
+public interface BinarySearch {
 
-    public static int binarySearch(int[] array, int target) {
-        // Write your code here.
-        return internalBinarySearch(array, target, 0, array.length-1);
-    }
+    int binarySearch(int[] array, int target);
 
-    private static int internalBinarySearch(int[] array, int target, int startIndex, int endIndex) {
-        if (array[startIndex] == target) return startIndex;
-        if (array[endIndex] == target) return endIndex;
-        if (endIndex-startIndex <= 1) return -1;
-        int middleIndex = (startIndex + endIndex) / 2;
-        if (target < array[middleIndex]) return internalBinarySearch(array, target, startIndex, middleIndex);
-        return internalBinarySearch(array, target, middleIndex, endIndex);
+    class Solution1 implements BinarySearch {
+
+        @Override
+        public int binarySearch(int[] array, int target) {
+            return internalBinarySearch(array, target, 0, array.length-1);
+        }
+
+        private int internalBinarySearch(int[] array, int target, int startIndex, int endIndex) {
+            if (array[startIndex] == target) return startIndex;
+            if (array[endIndex] == target) return endIndex;
+            if (endIndex-startIndex <= 1) return -1;
+            int middleIndex = (startIndex + endIndex) / 2;
+            if (target < array[middleIndex]) return internalBinarySearch(array, target, startIndex, middleIndex);
+            return internalBinarySearch(array, target, middleIndex, endIndex);
+        }
     }
 }
