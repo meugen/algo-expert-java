464e733191a873bbe45b8002bf8576fbdb2431e5
Yevhen Malyshev <yevhen.malyshev@oracle.com>
2022-09-28 21:56:44 +0300

    Refactored FindThreeLargestNumbers task

--- src/main/java/com/algo/expert/searching/FindThreeLargestNumbers.java
+++ src/main/java/com/algo/expert/searching/FindThreeLargestNumbers.java
@@ -2,21 +2,25 @@ package com.algo.expert.searching;
 
 import java.util.Arrays;
 
-public class FindThreeLargestNumbers {
+public interface FindThreeLargestNumbers {
 
-    public static int[] findThreeLargestNumbers(int[] array) {
-        // Write your code here.
-        int [] result = new int[] { Integer.MIN_VALUE, Integer.MIN_VALUE, Integer.MIN_VALUE };
-        for (int value : array) {
-            for (int i=0; i<result.length; i++) {
-                if (value > result[i]) {
-                    result[i] = value;
-                    Arrays.sort(result);
-                    break;
+    int[] findThreeLargestNumbers(int[] array);
+
+    class Solution1 implements FindThreeLargestNumbers {
+
+        @Override
+        public int[] findThreeLargestNumbers(int[] array) {
+            int [] result = new int[] { Integer.MIN_VALUE, Integer.MIN_VALUE, Integer.MIN_VALUE };
+            for (int value : array) {
+                for (int i=0; i<result.length; i++) {
+                    if (value > result[i]) {
+                        result[i] = value;
+                        Arrays.sort(result);
+                        break;
+                    }
                 }
             }
+            return result;
         }
-        //Arrays.sort(result);
-        return result;
     }
 }
