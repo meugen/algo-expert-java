a55172cef9595dac3a30bb16848c816023227d1f
Yevhen Malyshev <yevhen.malyshev@oracle.com>
2022-09-28 22:38:28 +0300

    Refactored RunLengthEncoding task

--- src/main/java/com/algo/expert/strings/RunLengthEncoding.java
+++ src/main/java/com/algo/expert/strings/RunLengthEncoding.java
@@ -1,22 +1,27 @@
 package com.algo.expert.strings;
 
-public class RunLengthEncoding {
+public interface RunLengthEncoding {
 
-    public static String runLengthEncoding(String string) {
-        // Write your code here.
-        StringBuilder builder = new StringBuilder();
-        int startIndex = 0;
-        char currentChar = string.charAt(0);
-        for (int i=1; i<string.length(); i++) {
-            if (string.charAt(i) != currentChar || i - startIndex + 1 >= 10) {
-                builder.append(i - startIndex);
-                builder.append(currentChar);
-                startIndex = i;
-                currentChar = string.charAt(i);
+    String runLengthEncoding(String string);
+
+    class Solution1 implements RunLengthEncoding {
+
+        @Override
+        public String runLengthEncoding(String string) {
+            StringBuilder builder = new StringBuilder();
+            int startIndex = 0;
+            char currentChar = string.charAt(0);
+            for (int i=1; i<string.length(); i++) {
+                if (string.charAt(i) != currentChar || i - startIndex + 1 >= 10) {
+                    builder.append(i - startIndex);
+                    builder.append(currentChar);
+                    startIndex = i;
+                    currentChar = string.charAt(i);
+                }
             }
+            builder.append(string.length() - startIndex);
+            builder.append(currentChar);
+            return builder.toString();
         }
-        builder.append(string.length() - startIndex);
-        builder.append(currentChar);
-        return builder.toString();
     }
 }
