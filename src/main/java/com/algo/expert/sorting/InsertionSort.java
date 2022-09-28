d1b1296c067c01c1689361a354d2cf742ceb84a7
Yevhen Malyshev <yevhen.malyshev@oracle.com>
2022-09-28 22:23:21 +0300

    Refactored InsertionSort task

--- src/main/java/com/algo/expert/sorting/InsertionSort.java
+++ src/main/java/com/algo/expert/sorting/InsertionSort.java
@@ -1,17 +1,23 @@
 package com.algo.expert.sorting;
 
-public class InsertionSort {
+public interface InsertionSort {
 
-    public static int[] insertionSort(int[] array) {
-        for (int i=1; i<array.length; i++) {
-            int index = i;
-            while (index>0 && array[index]<array[index-1]) {
-                int tmp = array[index-1];
-                array[index-1] = array[index];
-                array[index] = tmp;
-                index--;
+    int[] insertionSort(int[] array);
+
+    class Solution1 implements InsertionSort {
+
+        @Override
+        public int[] insertionSort(int[] array) {
+            for (int i=1; i<array.length; i++) {
+                int index = i;
+                while (index>0 && array[index]<array[index-1]) {
+                    int tmp = array[index-1];
+                    array[index-1] = array[index];
+                    array[index] = tmp;
+                    index--;
+                }
             }
+            return array;
         }
-        return array;
     }
 }
