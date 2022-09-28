af048c04d275fd689d426325127bec23003ba746
Yevhen Malyshev <yevhen.malyshev@oracle.com>
2022-09-28 20:47:05 +0300

    Refactored FindClosestValueBst task

--- src/main/java/com/algo/expert/binarysearchtrees/FindClosestValueBst.java
+++ src/main/java/com/algo/expert/binarysearchtrees/FindClosestValueBst.java
@@ -1,22 +1,26 @@
 package com.algo.expert.binarysearchtrees;
 
-public class FindClosestValueBst {
+public interface FindClosestValueBst {
 
-    private FindClosestValueBst() {}
+    int findClosestValueInBst(BST tree, int target);
 
-    public static int findClosestValueInBst(BST tree, int target) {
-        return findClosestCheckDiffer(tree, target, target-tree.value);
+    class Solution1 implements FindClosestValueBst {
+
+        @Override
+        public int findClosestValueInBst(BST tree, int target) {
+            return findClosestCheckDiffer(tree, target, target-tree.value);
+        }
+
+        private int findClosestCheckDiffer(BST tree, int target, int closestDiffer) {
+            if (tree.value==target) return target;
+            if (Math.abs(target-tree.value) < Math.abs(closestDiffer)) closestDiffer = target-tree.value;
+            if (target<tree.value && tree.left!=null) return findClosestCheckDiffer(tree.left, target, closestDiffer);
+            if (target>tree.value && tree.right!=null) return findClosestCheckDiffer(tree.right, target, closestDiffer);
+            return target - closestDiffer;
+        }
     }
 
-    private static int findClosestCheckDiffer(BST tree, int target, int closestDiffer) {
-        if (tree.value==target) return target;
-        if (Math.abs(target-tree.value) < Math.abs(closestDiffer)) closestDiffer = target-tree.value;
-        if (target<tree.value && tree.left!=null) return findClosestCheckDiffer(tree.left, target, closestDiffer);
-        if (target>tree.value && tree.right!=null) return findClosestCheckDiffer(tree.right, target, closestDiffer);
-        return target - closestDiffer;
-    }
-
-    static class BST {
+    class BST {
         public int value;
         public BST left;
         public BST right;
@@ -25,4 +29,5 @@ public class FindClosestValueBst {
             this.value = value;
         }
     }
+
 }
