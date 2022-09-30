8471cda0c62c62269415769f20c156d9e990eee7
Yevhen Malyshev <yevhen.malyshev@oracle.com>
2022-09-30 18:57:12 +0300

    Intermediate commit

--- src/main/java/com/algo/expert/binarysearchtrees/BstConstruction.java
+++ src/main/java/com/algo/expert/binarysearchtrees/BstConstruction.java
@@ -12,15 +12,71 @@ public interface BstConstruction {
         }
 
         public BST insert(int value) {
+            if (value < this.value) {
+                if (left == null) {
+                    left = new BST(value);
+                } else {
+                    left.insert(value);
+                }
+                return this;
+            }
+
+            if (value > this.value) {
+                if (right == null) {
+                    right = new BST(value);
+                } else {
+                    right.insert(value);
+                }
+                return this;
+            }
+
+            BST right = this.right;
+            this.right = new BST(value);
+            this.right.right = right;
             return this;
         }
 
         public BST remove(int value) {
+            if (value < this.value) {
+                if (left != null) {
+                    left.remove(value);
+                }
+                return this;
+            }
+
+            if (value > this.value) {
+                if (right != null) {
+                    right.remove(value);
+                }
+                return this;
+            }
+
+            if (right != null) {
+                BST oldLeft = this.left;
+                this.value = right.value;
+                this.left = right.left;
+                this.right = right.right;
+                BST latestLeft = this;
+                while (latestLeft.left != null) {
+                    latestLeft = latestLeft.left;
+                }
+                latestLeft.left = oldLeft;
+            } else if (left != null) {
+                this.value = left.value;
+                this.right = left.right;
+                this.left = left.left;
+            }
             return this;
         }
 
         public boolean contains(int value) {
-            return false;
+            if (value < this.value) {
+                return left != null && left.contains(value);
+            }
+            if (value > this.value) {
+                return right != null && right.contains(value);
+            }
+            return true;
         }
     }
 }
