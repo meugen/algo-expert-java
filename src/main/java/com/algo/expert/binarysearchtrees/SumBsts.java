08eeb8064111aea9f31e6820f5da1d99992a3d62
Yevhen Malyshev <meugen.82@gmail.com>
2025-10-16 16:40:01 +0000

    Fixed Sum BSTs algorithm

--- src/main/java/com/algo/expert/binarysearchtrees/SumBsts.java
+++ src/main/java/com/algo/expert/binarysearchtrees/SumBsts.java
@@ -18,25 +18,40 @@ public interface SumBsts {
 
         @Override
         public int sumBsts(BinaryTree tree) {
-            return buildResult(tree, Integer.MIN_VALUE, Integer.MAX_VALUE).sum;
+            var result = buildResult(tree);
+            return result.count >= 3 ? result.sum : 0;
         }
 
-        private Result buildResult(BinaryTree tree, int min, int max) {
-            if (tree == null) return new Result(true, 0);
-            Result leftResult = buildResult(tree.left, min, tree.value);
-            Result rightResult = buildResult(tree.right, tree.value, max);
-            boolean valid = leftResult.valid && rightResult.valid;
-            int sum = (valid ? tree.value : 0) + leftResult.sum + rightResult.sum; 
-            return new Result(valid, sum);
+        private Result buildResult(BinaryTree tree) {
+            if (tree == null) return null;
+            var leftResult = buildResult(tree.left);
+            var rightResult = buildResult(tree.right);
+            var valid = (leftResult == null || (leftResult.valid && leftResult.min < tree.value &&  leftResult.max < tree.value))
+                && (rightResult == null || (rightResult.valid && tree.value <= rightResult.min && tree.value <= rightResult.max));
+            var min = Math.min(tree.value, leftResult == null ? Integer.MAX_VALUE : leftResult.min);
+            var max = Math.max(tree.value, rightResult == null ? Integer.MIN_VALUE : rightResult.max);
+            var sum = valid ? tree.value : 0;
+            sum += leftResult == null || (!valid && leftResult.count < 3) ? 0 : leftResult.sum;
+            sum += rightResult == null || (!valid && rightResult.count < 3) ? 0 : rightResult.sum;
+            var count = 1;
+            count += leftResult == null ? 0 : leftResult.count;
+            count += rightResult == null ? 0 : rightResult.count;
+            return new Result(valid, min, max, sum, count);
         }
 
         private static class Result {
             final boolean valid;
+            final int min;
+            final int max;
             final int sum;
+            final int count;
 
-            Result(boolean valid, int sum) {
+            Result(boolean valid, int min, int max, int sum, int count) {
                 this.valid = valid;
+                this.min = min;
+                this.max = max;
                 this.sum = sum;
+                this.count = count;
             }
         }
     }
